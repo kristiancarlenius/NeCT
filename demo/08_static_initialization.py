40ef004aad1838a90f315274013d25673e7857d1
kristiancarlenius <kristian.carlenius@hotmail.com>
2025-09-22 18:22:52 +0200

    dynamic epoch saved

--- demo/08_static_initialization.py
+++ demo/08_static_initialization.py
@@ -4,25 +4,17 @@ import numpy as np
 import nect
 
 data_path = "/cluster/home/kristiac/NeCT/Datasets/bentheimer/"
+"""
 config_file = Path(data_path) / "config.yaml"
-geometry_file = Path(data_path) / "geometry.yaml"
-
-# load config
 with open(config_file, "r") as f:
     config = yaml.safe_load(f)
-
-# update img_path to point to projections folder
 config["img_path"] = str(Path(data_path) / "projections")
-
-# write the updated config to a temporary yaml
 tmp_config_file = Path(data_path) / "config_tmp.yaml"
 with open(tmp_config_file, "w") as f:
     yaml.safe_dump(config, f)
-
-# export dataset to npy (requires the helper function we discussed earlier)
 nect.export_dataset_to_npy(tmp_config_file, Path(data_path) / "projections.npy")
-
-# load geometry
+"""
+geometry_file = Path(data_path) / "geometry.yaml"
 geometry = nect.Geometry.from_yaml(geometry_file)
 
 # run reconstruction using the new .npy projections
@@ -32,7 +24,7 @@ reconstruction_path = nect.reconstruct(
     quality="high",
     mode="dynamic",
     config_override={
-        "epochs": "3x",
+        "epochs": "9x",
         "checkpoint_interval": 0,
         "image_interval": 0,
         "plot_type": "XZ",
