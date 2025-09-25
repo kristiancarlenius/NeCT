99124224b4980aebf64e72bb3eaec131a9b5271a
kristiancarlenius <kristian.carlenius@hotmail.com>
2025-09-25 16:25:36 +0200

    matching encoders and network

--- demo/08_static_initialization.py
+++ demo/08_static_initialization.py
@@ -18,37 +18,45 @@ nect.export_dataset_to_npy(tmp_config_file, Path(data_path) / "projections.npy")
 geometry_file = Path(data_path) / "geometry.yaml"
 geometry = nect.Geometry.from_yaml(geometry_file)
 
-"""
+
 # run reconstruction using the new .npy projections
-reconstruction_path = nect.reconstruct(
+reconstruction_path_static = nect.reconstruct(
     geometry=geometry,
     projections=str(Path(data_path) / "projections.npy"),
     quality="high",
     mode="static",
-    exp_name="static_initilalization",
+    exp_name="static_init",
     config_override={
-        "epochs": "2x",
+        "epochs": "1.5x",
         "checkpoint_interval": 0,
         "image_interval": 0,
         "plot_type": "XZ",
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 20,
+            "n_levels": 21,
             "n_features_per_level": 4,
             "log2_hashmap_size": 21,
             "base_resolution": 16,
             "max_resolution_factor": 2,
         },
+        "net": {
+            "otype": "FullyFusedMLP",
+            "activation": "LeakyReLU",
+            "output_activation": None,
+            "n_neurons": 128,
+            "n_hidden_layers": 4,
+            "include_identity": True,
+        },
     },
 )
-"""
 
-reconstruction_path = nect.reconstruct(
+"""
+reconstruction_path_dynamic = nect.reconstruct(
     geometry=geometry,
     projections=str(Path(data_path) / "projections.npy"),
     quality="high",
     mode="dynamic",
-    exp_name="dynamic_initilalized",
+    exp_name="dynamic_init",
     static_init = "/cluster/home/kristiac/NeCT/outputs/static_initilalization/hash_grid_20_4_21_16_2_6_64_L1/2025-09-24T10-44-45/model/checkpoints/last.ckpt",
     static_init_config="/cluster/home/kristiac/NeCT/outputs/static_initilalization/hash_grid_20_4_21_16_2_6_64_L1/2025-09-24T10-44-45/model/config.yaml",
     config_override={
@@ -58,14 +66,23 @@ reconstruction_path = nect.reconstruct(
         "plot_type": "XZ",
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 20,
+            "n_levels": 21,
             "n_features_per_level": 4,
             "log2_hashmap_size": 21,
             "base_resolution": 16,
             "max_resolution_factor": 2,
         },
+        "net": {
+            "otype": "FullyFusedMLP",
+            "activation": "LeakyReLU",
+            "output_activation": None,
+            "n_neurons": 128,
+            "n_hidden_layers": 4,
+            "include_identity": True,
+        },
     },
 )
-nect.export_volumes(reconstruction_path, binning=3, avg_timesteps=5)
 
+nect.export_volume(reconstruction_path_dynamic, binning=3, avg_timesteps=5)
+"""
 
