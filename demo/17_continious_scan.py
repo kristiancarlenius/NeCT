1e436aed675d5f40ceadfd633a5436366bd99864
kristiancarlenius <kristian.carlenius@hotmail.com>
2025-11-09 19:11:57 +0100

    longer!

--- demo/17_continious_scan.py
+++ demo/17_continious_scan.py
@@ -60,7 +60,7 @@ reconstruction_path_dynamic, _ = nect.reconstruct_continious_scan(
     mode="static",
     exp_name="static_continious",
     config_override={
-        "epochs": "5x",
+        "epochs": "8x",
         "checkpoint_interval": 0,
         "image_interval": 0,
         "plot_type": "XZ",
@@ -86,7 +86,7 @@ reconstruction_path_dynamic, _ = nect.reconstruct_continious_scan(
             include_identity=False,
             include_adaptive_skip=False,
         ),
-        "accumulation_steps": 5,
+        "accumulation_steps": 8,
         "continous_scanning": True,
         
     },)
