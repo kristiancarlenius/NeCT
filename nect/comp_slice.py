1473a6a3f02421a87c1272a67d40bae727d21fa0
kristiancarlenius <kristian.carlenius@hotmail.com>
2025-12-07 15:26:28 +0100

    path

--- nect/comp_slice.py
+++ nect/comp_slice.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 # CONFIG: EDIT THESE
 # ================================
 REF_PATH = r"/home/user/Documents/img_comp/pr360_ac6/1425_0360.png"   # path to your "perfect" image
-TEST_PATH = r"/home/user/Documents/img_comp/pr100_ac4/0100_0100.png" # path to your reconstruction image
+TEST_PATH = r"/home/user/Documents/img_comp/pr360_ac4/0500_0360.png" # path to your reconstruction image
 
 # Crop rectangle (in pixel coordinates)
 # (x0, y0) is top-left; (x1, y1) is bottom-right (exclusive)
