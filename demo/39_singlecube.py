16f26f44eb45f0ecdaeedc63c6dbb3cc4d83e98c
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-03-04 17:00:28 +0100

    single experiments

--- demo/39_singlecube.py
+++ demo/39_singlecube.py
@@ -71,9 +71,9 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         },
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 23,
+            "n_levels": 27,
             "n_features_per_level": 4,
-            "log2_hashmap_size": 23,
+            "log2_hashmap_size": 27,
             "base_resolution": 32,
             "max_resolution_factor": 4,
         },
