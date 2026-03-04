83306e1d90b9a116f9de7ec6e64595bf706b2856
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-03-04 22:12:54 +0100

    slight reduction of single

--- demo/39_singlecube.py
+++ demo/39_singlecube.py
@@ -71,9 +71,9 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         },
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 27,
+            "n_levels": 25,
             "n_features_per_level": 4,
-            "log2_hashmap_size": 27,
+            "log2_hashmap_size": 25,
             "base_resolution": 32,
             "max_resolution_factor": 4,
         },
