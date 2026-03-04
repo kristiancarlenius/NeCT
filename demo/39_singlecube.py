9603988d5e164e2acb30f2d5c58cc5e62ce598b8
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-03-04 15:30:31 +0100

    resolution changes in singlecube

--- demo/39_singlecube.py
+++ demo/39_singlecube.py
@@ -71,11 +71,11 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         },
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 27,
+            "n_levels": 23,
             "n_features_per_level": 4,
-            "log2_hashmap_size": 28,
-            "base_resolution": 16,
-            "max_resolution_factor": 2,
+            "log2_hashmap_size": 23,
+            "base_resolution": 20,
+            "max_resolution_factor": 3,
         },
         "net": MLPNetConfig(
             otype="FullyFusedMLP",
