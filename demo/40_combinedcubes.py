bcb049236e1d1ba3f62ef77f807b3bcbca26b142
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-03-16 13:45:55 +0100

    testing hash vs levels

--- demo/40_combinedcubes.py
+++ demo/40_combinedcubes.py
@@ -71,9 +71,9 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         },
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 25,
+            "n_levels": 24,
             "n_features_per_level": 4,
-            "log2_hashmap_size": 24,
+            "log2_hashmap_size": 25,
             "base_resolution": 16,
             "max_resolution_factor": 2,
         },
