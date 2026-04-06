dde5df7a767bf415c3a2d317bb184228d80b9e69
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-04-06 15:25:31 +0200

    22_4_23

--- demo/28_normal_sizeby.py
+++ demo/28_normal_sizeby.py
@@ -71,9 +71,9 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         },
         "encoder": {
             "otype": "HashGrid",
-            "n_levels": 19,
+            "n_levels": 22,
             "n_features_per_level": 4,
-            "log2_hashmap_size": 20,
+            "log2_hashmap_size": 23,
             "base_resolution": 16,
             "max_resolution_factor": 2,
         },
