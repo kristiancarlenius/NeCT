066854a6a937a8e3efa010e04c06185f0aa1dce7
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-05-04 23:25:17 +0200

    hourglass test2

--- demo/60_hourglass_sand_volume.py
+++ demo/60_hourglass_sand_volume.py
@@ -34,7 +34,7 @@ from nect.sampling import Geometry
 MODEL_PATH = "/cluster/home/kristiac/NeCT/outputs/dynamic_continious/quadcubes_22_4_22_16_2_4_128_L1/4fps_2750_ac2_re/model/"
 
 # How many evenly-spaced timesteps to sample across the full acquisition
-N_TIMESTEPS = 80
+N_TIMESTEPS = 800
 
 # Spatial binning factor (4 = 4× faster/lower-res; 1 = full resolution)
 BINNING = 1
