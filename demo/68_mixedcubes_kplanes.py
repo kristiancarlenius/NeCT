c30376e62ccda8d3009b454771eaf30bbd78472c
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-05-22 01:08:46 +0200

    mixed kplane

--- demo/68_mixedcubes_kplanes.py
+++ demo/68_mixedcubes_kplanes.py
@@ -26,7 +26,7 @@ reconstruction_path_dynamic, _ = nect.reconstruct(
         "encoder": {
             "otype": "HashGrid",
             "n_levels": 20,
-            "n_features_per_level": 2,
+            "n_features_per_level": 4,
             "log2_hashmap_size": 24,
             "base_resolution": 16,
             "max_resolution_factor": 2,
