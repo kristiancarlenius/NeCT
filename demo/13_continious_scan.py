0df17459ce4f65c87cf400b93e2bf5d0cf9eecf2
kristiancarlenius <kristian.carlenius@hotmail.com>
2026-06-06 15:42:03 +0200

    continous

--- demo/13_continious_scan.py
+++ demo/13_continious_scan.py
@@ -99,7 +99,7 @@ reconstruction_path_dynamic, _ = nect.reconstruct_continious_scan(
             "include_identity": False,
         },
         "tv_spatial": 1e-4,
-        "accumulation_steps": 3,
+        "accumulation_steps": 6,
         "continous_scanning": True,
         
     },
